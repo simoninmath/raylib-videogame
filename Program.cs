039af8593e3ea2e1399f22fde33631bce8754182
SIMONIN Mathieu <simonin.math@gmail.com>
2025-02-08 18:05:51 +0100

    #RLVG [Back] - Update Program.cs class with windows configurations

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Raylib_cs;
 using static Raylib_cs.Raylib;
+using static Raylib_cs.ConfigFlags;
 
 public class RaylibGame
 {
@@ -7,13 +8,33 @@ public class RaylibGame
   public static int Main()
   {
       System.Console.WriteLine("Checking Raylib!");
-      InitWindow(1024, 768, "Raylib Video Game");
 
+      // Set the screen size to the current monitor size
+      int screen = GetCurrentMonitor();
+      int screenWidth = GetMonitorWidth(screen);
+      int screenHeight = GetMonitorHeight(screen);
+
+      InitWindow(screenWidth, screenHeight, "Raylib Video Game");
+
+      // Set the window size and styles
+      SetWindowState(BorderlessWindowMode);
+      // SetWindowSize(screenWidth, screenHeight);
+
+      // Allow responsive screen size
+      int width = GetScreenWidth();
+      int height = GetScreenHeight();
+      
       while (!WindowShouldClose())
       {
           BeginDrawing();
           ClearBackground(Color.White);
-          DrawText("GAME OVER AGAIN!", 190, 200, 20, Color.Red);
+
+          if(IsWindowState(UnfocusedWindow))
+          {
+              DrawText("GAME PAUSED", 190, 200, 20, Color.Red);
+          }
+
+          DrawText("COCO !", 190, 200, 20, Color.Red);
           EndDrawing();
       }
 
