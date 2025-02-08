822cc75b7ff37a89a5ddd7e6e6a5c3d4987146d1
SIMONIN Mathieu <simonin.math@gmail.com>
2025-02-08 13:04:12 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+public class RaylibGame
+{
+
+  public static int Main()
+  {
+      System.Console.WriteLine("Checking Raylib");
+      InitWindow(1024, 768, "Raylib Video Game");
+
+      while (!WindowShouldClose())
+      {
+          BeginDrawing();
+          ClearBackground(Color.White);
+          DrawText("GAME OVER!", 190, 200, 20, Color.Red);
+          EndDrawing();
+      }
+
+      CloseWindow();
+      return 0;
+  }
+}
