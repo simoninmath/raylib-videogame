505808128f828367549d897cc81495c89eaa1cfb
SIMONIN Mathieu <simonin.math@gmail.com>
2025-02-09 10:21:38 +0100

    #RLVG [Back] - Updated Progam.cs with drawing methods

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using Raylib_cs;
 using static Raylib_cs.Raylib;
 using static Raylib_cs.ConfigFlags;
+using System.Numerics;
 
 public class RaylibGame
 {
-
   public static int Main()
   {
+      float rotation = 0.0f;
       System.Console.WriteLine("Checking Raylib!");
 
       // Set the screen size to the current monitor size
@@ -18,7 +19,6 @@ public class RaylibGame
 
       // Set the window size and styles
       SetWindowState(BorderlessWindowMode);
-      // SetWindowSize(screenWidth, screenHeight);
 
       // Allow responsive screen size
       int width = GetScreenWidth();
@@ -26,15 +26,27 @@ public class RaylibGame
       
       while (!WindowShouldClose())
       {
-          BeginDrawing();
-          ClearBackground(Color.White);
+          rotation += 0.5f;
 
+          BeginDrawing();
+          ClearBackground(Color.DarkBlue);
+
+          // Check if the window is focused
           if(IsWindowState(UnfocusedWindow))
           {
               DrawText("GAME PAUSED", 190, 200, 20, Color.Red);
           }
 
-          DrawText("COCO !", 190, 200, 20, Color.Red);
+          // DrawTriangle(new Vector2(600, 600), new Vector2(650, 650), new Vector2(700, 700), Color.Magenta);
+
+          DrawLine(10, 10, 100, 100, Color.Red);
+          DrawCircle(200, 200, 50, Color.Blue);
+          DrawCircleV(new Vector2(300, 300), 50, Color.Green);
+          DrawCircleLines(400, 400, 50, Color.Purple);  
+          DrawRectangle(500, 500, 50, 50, Color.Yellow);
+          DrawPoly(new Vector2(600, 600), 6, 50, rotation, Color.Orange);
+
+          DrawText("Hi!", 190, 200, 20, Color.Red);
           EndDrawing();
       }
 
